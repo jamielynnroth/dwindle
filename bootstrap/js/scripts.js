bab65d43588c189184365c16213d5e43745b40a2
Jamie Roth <roth.j@me.com>
2014-02-04 14:27:09 -0500

    fix bug in create page

--- bootstrap/js/scripts.js
+++ bootstrap/js/scripts.js
@@ -1,53 +1,58 @@
-$('a[href$="aboutus"]').click(function () {
-	$('#aboutus-popup').removeClass('hidden');
-	$('.closeanywhere').removeClass('hidden');
-	$('#how-popup').addClass('hidden');
-	$('#ss-form').hide();
-	// $('.full-logo').css({'top': '-385px', 'position': 'relative'});
-	$('#tagline').hide();
-	$('.instructions').hide();
-});
-
-$('.fui-cross').click(function() {
-	$('#aboutus-popup').addClass('hidden');
-	$('#how-popup').addClass('hidden');
-	$('.closeanywhere').addClass('hidden');
-	$('#ss-form').show();
-	// $('.full-logo').css({'top': '0'});
-	$('#tagline').show();
-	$('.instructions').show();
-})
-
-$('.closeanywhere').click(function() {
-	$('#aboutus-popup').addClass('hidden');
-	$('#how-popup').addClass('hidden');
-	$('#ss-form').show();
-	// $('.full-logo').css({'top': '0'});
-	$('#tagline').show();
-	$('.instructions').show();
-})
-
-
-$('a[href$="how"]').click(function () {
-	$('#how-popup').removeClass('hidden');
-	$('#aboutus-popup').addClass('hidden');
-	$('.closeanywhere').removeClass('hidden');
-	$('#ss-form').hide();
-	// $('.full-logo').css({'top': '-385px', 'position': 'relative'});
-	$('#tagline').hide();
-	$('.instructions').hide();
-});
-
-
-// $('.modal').hasClass('in').css('display','block');
-
-
 
 $(document).ready(function(){
+
+	$('a[href$="aboutus"]').click(function () {
+		$('#aboutus-popup').removeClass('hidden');
+		$('.closeanywhere').removeClass('hidden');
+		$('#how-popup').addClass('hidden');
+		$('#ss-form').hide();
+		$('#tagline').hide();
+		$('.instructions').hide();
+	});
+
+	$('.fui-cross').click(function() {
+		$('#aboutus-popup').addClass('hidden');
+		$('#how-popup').addClass('hidden');
+		$('.closeanywhere').addClass('hidden');
+		$('#ss-form').show();
+		$('#tagline').show();
+		$('.instructions').show();
+	})
+
+	$('.closeanywhere').click(function() {
+		$('#aboutus-popup').addClass('hidden');
+		$('#how-popup').addClass('hidden');
+		$('#ss-form').show();
+		$('#tagline').show();
+		$('.instructions').show();
+	})
+
+
+	$('a[href$="how"]').click(function () {
+		$('#how-popup').removeClass('hidden');
+		$('#aboutus-popup').addClass('hidden');
+		$('.closeanywhere').removeClass('hidden');
+		$('#ss-form').hide();
+		$('#tagline').hide();
+		$('.instructions').hide();
+	});
+
+
+
+
     $('#ss-form').submit(function(e) {
-    	
 		 e.preventDefault;
 		 $('#ss-form').hide();
+		 $('.instructions').hide();
+		 $('#tagline').hide();
+		 $('.full-logo').hide();
+		 $('#thanks').removeClass();
+		 return false;
+	});
+
+	$('#ss-form-2').submit(function(e) {
+		 e.preventDefault;
+		 $('#ss-form-2').hide();
 		 $('.instructions').hide();
 		 $('#tagline').hide();
 		 $('.full-logo').hide();
