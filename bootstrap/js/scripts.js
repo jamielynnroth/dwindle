7fa787444e98b727353091057e676d544c1f7a78
Jamie Roth <roth.j@me.com>
2014-02-04 02:53:53 -0500

    link up forms and add success pages

--- bootstrap/js/scripts.js
+++ bootstrap/js/scripts.js
@@ -3,4 +3,24 @@
 // 	$('#about-popup').removeClass('invisible');
 // });
 
-$('.modal').hasClass('in').css('display','block');
+// $('.modal').hasClass('in').css('display','block');
+
+
+// $('#aboutus').click(function() {
+// 	$('#myModal').modal(toggle);
+// });
+
+
+$(document).ready(function(){
+    $('#ss-form').submit(function(e) {
+    	//return false;
+		 e.preventDefault;
+		 $('#ss-form').hide();
+		 $('.instructions').hide();
+		 $('#tagline').hide();
+		 $('.full-logo').hide();
+		 $('#thanks').removeClass();
+	});
+
+});
+
