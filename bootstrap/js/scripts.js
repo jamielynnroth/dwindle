d9f2307a87aa35f54d764b08e1be4f2c40bb3c27
Jamie Roth <roth.j@me.com>
2014-02-28 01:43:51 -0500

    remove second sign up page, add 123, waitlist page

--- bootstrap/js/scripts.js
+++ bootstrap/js/scripts.js
@@ -1,14 +1,13 @@
-
 $(document).ready(function(){
 
-	$('a[href$="aboutus"]').click(function () {
+	$('a[href$="aboutus"]').click(function (e) {
+		e.preventDefault();
 		$('#aboutus-popup').removeClass('hidden');
 		$('.closeanywhere').removeClass('hidden');
-		// $('#thanks').addClass('hidden');
 		$('#how-popup').addClass('hidden');
-		$('#ss-form').hide();
-		$('#tagline').hide();
-		$('.instructions').hide();
+		// $('#ss-form').hide();
+		// $('#tagline').hide();
+		// $('.instructions').hide();
 	});
 
 	$('.fui-cross').click(function() {
@@ -18,7 +17,7 @@ $(document).ready(function(){
 		$('#ss-form').show();
 		$('#tagline').show();
 		$('.instructions').show();
-	})
+	});
 
 	$('.closeanywhere').click(function() {
 		$('#aboutus-popup').addClass('hidden');
@@ -28,39 +27,26 @@ $(document).ready(function(){
 		$('.instructions').show();
 		$('.full-logo').show();
 		$('.closeanywhere').addClass('hidden');
-	})
+	});
 
 
-	$('a[href$="how"]').click(function () {
+	$('a[href$="how"]').click(function (e) {
+		e.preventDefault();
 		$('#how-popup').removeClass('hidden');
 		$('#aboutus-popup').addClass('hidden');
-		// $('#thanks').addClass('hidden');
 		$('.closeanywhere').removeClass('hidden');
-		$('#ss-form').hide();
-		$('#tagline').hide();
-		$('.instructions').hide();
+		// $('#ss-form').hide();
+		// $('#tagline').hide();
+		// $('.instructions').hide();
 	});
 
 
 
 
-    $('#ss-form').submit(function(e) {
-    	 e.stopPropagation();
-		 e.preventDefault;
-		 $('#ss-form').hide();
-		 $('.instructions').hide();
-		 $('#tagline').hide();
-		 $('.full-logo').hide();
-		 $('#thanks').removeClass();
-		 $('nav').hide();
-		 $('.home-link').removeClass('hidden');
-
-		 //return false;
-	});
 
 	$('#ss-form-2').submit(function(e) {
 		 e.stopPropagation();
-		 e.preventDefault;
+		 e.preventDefault();
 		 $('#ss-form-2').hide();
 		 $('.instructions').hide();
 		 $('#tagline').hide();
@@ -68,9 +54,55 @@ $(document).ready(function(){
 		 $('#thanks').removeClass();
 		 $('nav').hide();
 		 $('.home-link').removeClass('hidden');
-
-		 //return false;
 	});
 
-});
+	$(this).submit(function(e) {
+		 e.stopPropagation();
+		 e.preventDefault();
+		 $('#ss-form').hide();
+		 $('.instructions').hide();
+		 $('#tagline').hide();
+		 $('.full-logo').hide();
+		 $('#thanks').removeClass();
+		 $('nav').hide();
+		 $('#how-steps').hide();
+		 $('.home-link').removeClass('hidden');
+	});
 
+// Initialize bootstrap tooltip API
+ $("[rel=tooltip]").tooltip();
+
+// happy.js initialization for signup form validation
+
+// function happyEmail () {
+// 	var reg = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/
+// 	if (reg.test(email)) {
+// 	return true; 
+// 	}
+// 	else {
+// 	return false;
+// 	} 
+// };
+
+
+
+// $('#ss-form').isHappy({
+//           fields: {
+//             // reference the field you're talking about, probably by `id`
+//             // but you could certainly do $('[name=name]') as well.
+//             '#entry_1697004160': {
+//               required: true,
+//               message: 'Please provide your ConEd.com username'
+//             },
+//             '#entry_1581237788': {
+//               required: true,
+//               message: "Please provide your ConEd.com password - we'll never share it."
+//             },
+//             '#entry_1063886520': {
+//               required: true,
+//               message: "Please provide your email so we can share your savings!"
+//               test: happy.email
+//             }
+//           }
+//         });
+});
